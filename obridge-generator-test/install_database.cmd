c3bd2bf0f274023326d3a5f113675227d1cfad1d
karsany <karsany@karsany.hu>
2017-01-18 01:56:26 +0100

    plsql test case for blob type and parameter issue #27

--- obridge-generator-test/install_database.cmd
+++ obridge-generator-test/install_database.cmd
@@ -6,3 +6,4 @@ sqlplus %database_connection% @../obridge-main/src/test/plsql/02_test_package.pc
 sqlplus %database_connection% @../obridge-main/src/test/plsql/03_test_procedure.prc
 sqlplus %database_connection% @../obridge-main/src/test/plsql/04_exec_function.fnc
 sqlplus %database_connection% @../obridge-main/src/test/plsql/05_plsql_type_example.pck
+sqlplus %database_connection% @../obridge-main/src/test/plsql/06_blob_test.pck
