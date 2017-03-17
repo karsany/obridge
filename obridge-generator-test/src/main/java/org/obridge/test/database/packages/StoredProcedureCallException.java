2987a609a36adba734dc739ac9dcfde3042a7348
karsany <karsany@karsany.hu>
2017-03-17 15:43:59 +0100

    generated codes deleted

--- obridge-generator-test/src/main/java/org/obridge/test/database/packages/StoredProcedureCallException.java
+++
@@ -1,9 +0,0 @@
-package org.obridge.test.database.packages;
-
-import java.sql.SQLException;
-
-public class StoredProcedureCallException extends RuntimeException {
-    public StoredProcedureCallException(SQLException e) {
-        throw new RuntimeException(e.getMessage(), e);
-    }
-}
